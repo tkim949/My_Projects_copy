91c6c8c29717d0bc1f9693c89c1f181dda7140ae
Owl <40027596+tkim949@users.noreply.github.com>
2020-09-10 16:36:53 -0700

    Add build folder (for copy repo)

---
+++ src/build/precache-manifest.ef5d307ed8cf89230083447706a0b8c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bd65ae04e9924819a7b2f02dfcb692",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4de6d773a2be9d88ac09",
+    "url": "/static/css/main.69891b90.chunk.css"
+  },
+  {
+    "revision": "042d67ffcf498034448d",
+    "url": "/static/js/2.f85cad13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f85cad13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4de6d773a2be9d88ac09",
+    "url": "/static/js/main.7c1fad54.chunk.js"
+  },
+  {
+    "revision": "e1f4633d0b250b75e305",
+    "url": "/static/js/runtime-main.2cb59220.js"
+  },
+  {
+    "revision": "5659a3dfabe86ee2d09201276fe8a609",
+    "url": "/static/media/Resume_TaekyoungKim_0627.5659a3df.pdf"
+  },
+  {
+    "revision": "23abed93c19e05abd9f5f1b515f3d2dd",
+    "url": "/static/media/resume_pic_0627.23abed93.PNG"
+  }
+]);
